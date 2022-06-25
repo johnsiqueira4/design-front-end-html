168cb3b8397476105b9179b759d148acb9aa3bb8
jonatha <brenosiqueira.4ndroid@gmail.com>
2022-06-24 21:42:48 -0300

    finishing code aula gravada 03/06/2022

---
+++ js/curso.js
@@ -0,0 +1,20 @@
+console.log("curso front-end");
+let nome = prompt("Digite seu nome: ");
+
+console.log(nome);
+
+let idade = prompt("Qual sua idade?");
+
+if (idade >= 18) {
+  alert("Você pode entrar!");
+} else {
+  alert("Site não adequado para menores de 18 anos!");
+}
+
+let confirmacao = confirm("Continuar?");
+
+let texto = "maça, pera ";
+texto += "uva, abacaxi ";
+texto += "morango, goiaba, melancia!";
+
+console.log(texto);

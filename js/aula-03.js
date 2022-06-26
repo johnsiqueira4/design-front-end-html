77c264f451092f4191891ecdb99185e9f9181fde
jonatha <brenosiqueira.4ndroid@gmail.com>
2022-06-26 13:24:30 -0300

    finishing code aula gravada js 10/06/2022 part2

--- js/aula-03.js
+++ js/aula-03.js
@@ -1,23 +1,41 @@
 let alunos = ["Jonatha", "Breno", "Rafael", "José", "Sofia", "Maria"];
 
-let objetosAlunos = document.querySelectorAll("main ul li");
+// window.setTimeout(carregaAlunos, 5000);
 
-objetosAlunos.forEach(function (item, index, array) {
-  if (index <= 1) {
-    objetosAlunos[index].innerHTML =
-      index + 1 + (index === 0 ? "st: " : "nd: ") + alunos[index];
-    objetosAlunos[index].style.listStyle = "none";
-    index === 0
-      ? (objetosAlunos[index].style.background = "#00ff00")
-      : (objetosAlunos[index].style.background = "yellow");
-    objetosAlunos[index].style.fontSize = "40px";
-    objetosAlunos[index].style.fontWeight = "700";
-  } else {
-    objetosAlunos[index].innerHTML = index + 1 + "th: " + alunos[index];
-    objetosAlunos[index].style.listStyle = "none";
+window.setTimeout(function () {
+  carregaAlunos();
+}, 3000);
 
-    index % 2 === 0
-      ? (objetosAlunos[index].style.background = "#ccc")
-      : undefined;
-  }
-});
+// window.setInterval(function () {
+//   carregaAlunos();
+// }, 3000);
+
+function carregaAlunos() {
+  // let objetosAlunos = document.querySelectorAll("main ul li");
+  // objetosAlunos.forEach(function (item, index, array) {
+  alunos.forEach(function (item, index, array) {
+    let liNova = document.createElement("li");
+    let nomeAluno = document.createTextNode(alunos[index]);
+    liNova.appendChild(nomeAluno);
+
+    document.querySelector("main ul").appendChild(liNova);
+
+    //   if (index <= 1) {
+    //     objetosAlunos[index].innerHTML =
+    //       index + 1 + (index === 0 ? "st: " : "nd: ") + alunos[index];
+    //     objetosAlunos[index].style.listStyle = "none";
+    //     index === 0
+    //       ? (objetosAlunos[index].style.background = "#00ff00")
+    //       : (objetosAlunos[index].style.background = "yellow");
+    //     objetosAlunos[index].style.fontSize = "40px";
+    //     objetosAlunos[index].style.fontWeight = "700";
+    //   } else {
+    //     objetosAlunos[index].innerHTML = index + 1 + "th: " + alunos[index];
+    //     objetosAlunos[index].style.listStyle = "none";
+
+    //     index % 2 === 0
+    //       ? (objetosAlunos[index].style.background = "#ccc")
+    //       : undefined;
+    //   }
+  });
+}

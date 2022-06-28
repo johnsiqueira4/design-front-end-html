82b1368c6f81ce4a60f76a36e812f2cc5502cb4b
Jonatha <jbreno@techniys.com>
2022-06-28 00:11:47 -0300

    finished ultima aula js ajax 13/06/2022

--- js/ajax.js
+++ js/ajax.js
@@ -3,33 +3,53 @@
 
 function mostrarComentarios() {
 
-    var con = new XMLHttpRequest();
-    con.responseType = "json";
-    console.log(con);
+    var connection = new XMLHttpRequest();
+    connection.responseType = "json";
+    connection.open("GET","js/comentarios.json");
+    connection.send();
 
-    con.onreadystatechange = function() {
-        if(con.readyState == 4 && con.status == 200) {
-            let documento = con.response;
+    connection.onreadystatechange = function() {
+        if(connection.readyState == 4 && connection.status == 200) { /* passa por outra status antes do 4 ex: carregando */
+            let documento = connection.response;
             let ulComentarios = document.querySelector('article section ul');
             documento.comentarios.forEach(function(item, index, array){
-                let novaLi = document.createElement('li');
-                novaLi.append(item.texto);
-                ulComentarios.append(novaLi);
+
+
+                    let novaLi = document.createElement('li');
+                    let spanAutor = document.createElement('span');
+                    let spanSeguidores = document.createElement('span');
+                    let spanTexto = document.createElement('span');
+                    spanTexto.append(item.texto);
+                    spanAutor.append(item.autor);
+                    spanSeguidores.className = "seguidores";
+                    spanTexto.className = "texto"
+                    spanSeguidores.append(item.seguidores + " seguidores");
+                    if (index == 0) {
+                        novaLi.className = "primeira";
+                    }
+                    novaLi.append(spanTexto);
+                    novaLi.append(spanAutor);
+                    novaLi.append(spanSeguidores);
+
+                    ulComentarios.append(novaLi);
+                
+
             });
         }
+        else if (connection.status == 404) {
+            window.alert("Não foi possível carregar os comentários");
+        }
     }
-
-    con.open("GET","js/comentarios.json", true);
-    con.send();
 
     document.querySelector('article section').style.display = "block";
     document.querySelector('article button').style.display = "none";
 
-
 }
 
 window.onload = function() {
+
     document.querySelector('article button').onclick = function(){
         mostrarComentarios();
-    }
+    };
+    
 }
